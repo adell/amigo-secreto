f91a2fed794604e468fe1b1330c3ef1e721209ab
Ademir Lima <adellima@gmail.com>
2019-08-18 17:38:29 -0300

    Add filds to campaign

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,29 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'members/create'
+
+  get 'members/destroy'
+
+  get 'members/update'
+
+  get 'campaigns/show'
+
+  get 'campaigns/index'
+
+  get 'campaigns/create'
+
+  get 'campaigns/update'
+
+  get 'campaigns/destroy'
+
+  get 'campaigns/raffle'
+
+  get 'pages/home'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 
   mount Sidekiq::Web => '/sidekiq'
 end
